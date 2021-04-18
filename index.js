38a22880b99b2e4c19ca550b63520f91dea5f7dd
hakthepak <hakx7820@mylaurier.ca>
2021-04-18 16:10:03 -0400

    heroku addition

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ module.exports = class extends Page {
             <h2>${oEntity.title}</h2>
             <p><img src="${oEntity.featured_image}" alt="${oEntity.title}"</p>
             <p>${oEntity.full_description}</p>
-            <form action="https://serene-taiga-04277.herokuapp.com/payment" method="post">
+            <form action="https://payment-assignment-3.herokuapp.com/payment/" method="post">
             <input type="hidden" name="title" value="${oEntity.title}" />
             <input type="hidden" name="price" value="21" />
             <input type="tel" placeholder="enter your number" name="telephone"/>
